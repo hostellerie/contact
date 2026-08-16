1cf83c8c04b9a888d5dd8a5b6254ce137c3e6d85
Ben <hostellerie.org@gmail.com>
2026-08-16 15:03:02 +0200

    Update French UTF-8 language file for Contact 2.0.7

--- language/french_france_utf-8.php
+++ language/french_france_utf-8.php
@@ -1,103 +1,67 @@
 <?php
-
-/* Reminder: always indent with 4 spaces (no tabs). */
-// +---------------------------------------------------------------------------+
-// | Contact Plugin 1.1                                                        |
-// +---------------------------------------------------------------------------+
-// | french_france_utf-8.php                                                   |
-// |                                                                           |
-// | FrenchEnglish language file                                               |
-// +---------------------------------------------------------------------------+
-// | Copyright (C) 2016 by the following authors:                              |
-// |                                                                           |
-// | Authors: Ben - ben AT geeklog DOT fr                                      |
-// +---------------------------------------------------------------------------+
-// | Created with the Geeklog Plugin Toolkit.                                  |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is free software; you can redistribute it and/or             |
-// | modify it under the terms of the GNU General Public License               |
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-// | GNU General Public License for more details.                              |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
-// +---------------------------------------------------------------------------+
-
-/**
-* @package Contact
-*/
-
-/**
-* Import Geeklog plugin messages for reuse
-*
-* @global array $LANG32
-*/
 global $LANG32;
 
-// +---------------------------------------------------------------------------+
-// | Array Format:                                                             |
-// | $LANGXX[YY]:  $LANG - variable name                                       |
-// |               XX    - specific array name                                 |
-// |               YY    - phrase id or number                                 |
-// +---------------------------------------------------------------------------+
-
 $LANG_CONTACT_1 = array(
-    'plugin_name'         => 'Contact',
-	'contact_from'        => 'Contact de',
-	'contact_form'        => 'Formulaire de contact',
-	'add_your_name'       => 'Ajouter votre nom',
-	'add_valid_address'   => 'Ajouter votre adresse email',
-	'name'                => 'Nom',
-	'email'               => 'Email',
-	'message'             => 'Message'
+    'plugin_name' => 'Contact',
+    'contact_from' => 'Contact depuis',
+    'contact_form' => 'Formulaire de contact',
+    'add_your_name' => 'Indiquez votre nom',
+    'add_valid_address' => 'Indiquez une adresse email valide',
+    'name' => 'Nom',
+    'email' => 'Email',
+    'subject' => 'Sujet',
+    'message' => 'Message',
+    'send' => 'Envoyer le message',
+    'success' => 'Message envoyé',
+    'error' => 'Envoi impossible',
+    'sent' => 'Merci. Votre message a bien été envoyé.',
+    'send_error' => 'Le message n’a pas pu être envoyé. Veuillez réessayer plus tard.',
+    'recipient_error' => 'Le destinataire configuré est indisponible.',
+    'name_required' => 'Veuillez indiquer votre nom.',
+    'email_invalid' => 'Veuillez indiquer une adresse email valide.',
+    'subject_required' => 'Veuillez indiquer un sujet.',
+    'message_required' => 'Veuillez saisir un message.',
+    'message_too_long' => 'Le message est trop long. Maximum : %d caractères.',
+    'security_token_error' => 'Le jeton de sécurité du formulaire est invalide ou a expiré. Veuillez réessayer.',
+    'privacy_required' => 'Veuillez accepter l’information relative à la confidentialité.',
+    'too_fast' => 'Le formulaire a été envoyé trop rapidement. Veuillez réessayer.',
+    'speedlimit' => 'Veuillez attendre %d secondes avant d’envoyer un nouveau message.',
+    'cc_description' => 'M’envoyer une copie de ce message',
+    'cc_intro' => 'Voici une copie du message que vous avez envoyé.',
+    'leave_empty' => 'Laissez ce champ vide',
+    'captcha_error' => 'La validation CAPTCHA/reCAPTCHA a échoué. Veuillez réessayer.',
+    'human_confirmation' => 'Je confirme que je souhaite envoyer ce message.',
+    'human_confirmation_required' => 'Veuillez confirmer que vous souhaitez envoyer ce message.',
+    'mail_sender' => "Expéditeur : %s <%s>"
 );
 
-
-// Localization of the Admin Configuration UI
-$LANG_configsections['contact'] = array(
-    'label' => 'Contact',
-    'title' => 'Contact configuration'
-);
-
+$LANG_configsections['contact'] = array('label' => 'Contact', 'title' => 'Configuration du plugin Contact');
 $LANG_confignames['contact'] = array(
-    'contactloginrequired' => 'Contact Login Required',
-    'hidecontactmenu' => 'Hide contact Menu Entry',
-	'showleftblocks1' => 'Show left blocks on plugin page',
-	'showrightblocks1' => 'Show right blocks on plugin page',
-	'contact_page' => 'Static page id for contact page header',
-	'contact_page_footer' => 'Static page id for contact page footer',
-	'use_contact_form' => 'Use contact form',
-	'form_recipient' => 'Recipient user UID',
-	'menu' => 'Menu item name',
-	'message' => 'Message on contact form',
-	'folder_name' => 'Name of the public folder for contact page',
+    'contactloginrequired' => 'Connexion requise',
+    'hidecontactmenu' => 'Masquer l’entrée Contact du menu',
+    'showleftblocks1' => 'Afficher les blocs de gauche',
+    'showrightblocks1' => 'Afficher les blocs de droite',
+    'menu' => 'Libellé du menu',
+    'message' => 'Texte d’introduction du formulaire',
+    'contact_page' => 'ID de la page statique avant le formulaire',
+    'contact_page_footer' => 'ID de la page statique après le formulaire',
+    'use_contact_form' => 'Activer le formulaire de contact',
+    'form_recipient' => 'UID de l’utilisateur destinataire',
+    'allow_cc' => 'Autoriser une copie à l’expéditeur',
+    'show_subject' => 'Afficher le champ Sujet',
+    'subject_prefix' => 'Préfixe du sujet des emails',
+    'protection_mode' => 'Protection anti-spam',
+    'min_submit_seconds' => 'Délai minimal avant envoi en secondes',
+    'max_message_length' => 'Longueur maximale du message (caractères)',
+    'privacy_enabled' => 'Exiger l’acceptation de la confidentialité',
+    'privacy_text' => 'Texte relatif à la confidentialité',
+    'privacy_url' => 'URL de la politique de confidentialité'
 );
-
-$LANG_configsubgroups['contact'] = array(
-    'sg_0' => 'Main settings',
-
-);
-
-$LANG_fs['contact'] = array(
-    'fs_01' => 'Access and template',
-    'fs_02' => 'Contact page'
-);
-
-// Note: entries 0, 1, and 12 are the same as in $LANG_configselects['Core']
+$LANG_configsubgroups['contact'] = array('sg_0' => 'Paramètres principaux');
+$LANG_fs['contact'] = array('fs_01' => 'Accès et affichage', 'fs_02' => 'Page de contact', 'fs_03' => 'Email et antispam', 'fs_04' => 'Confidentialité');
 $LANG_configselects['contact'] = array(
-    0 => array('True' => 1, 'False' => 0),
-    1 => array('True' => TRUE, 'False' => FALSE)
+    0 => array('Oui' => 1, 'Non' => 0),
+    1 => array('Oui' => true, 'Non' => false),
+    2 => array('Automatique' => 0, 'Honeypot + délai' => 1, 'Honeypot + délai + confirmation humaine' => 2, 'reCAPTCHA si disponible' => 3)
 );
-
-// Messages for the plugin upgrade
-$PLG_contact_MESSAGE3002 = $LANG32[9]; // "requires a newer version of Geeklog"
-
-?>
+$PLG_contact_MESSAGE3002 = isset($LANG32[9]) ? $LANG32[9] : 'Ce plugin nécessite une version plus récente de Geeklog.';
