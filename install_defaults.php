e291c3351f9c7077f6569ac546b898ed19089268
Ben <hostellerie.org@gmail.com>
2026-08-16 15:01:59 +0200

    Update Contact configuration defaults for 2.0.7

--- install_defaults.php
+++ install_defaults.php
@@ -1,126 +1,68 @@
 <?php
+/** Contact 2.0.7 - default configuration */
 
-/* Reminder: always indent with 4 spaces (no tabs). */
-// +---------------------------------------------------------------------------+
-// | Contact Plugin 1.2.0                                                      |
-// +---------------------------------------------------------------------------+
-// | functions.inc                                                             |
-// |                                                                           |
-// | This file does two things: 1) it implements the necessary Geeklog Plugin  |
-// | API methods and 2) implements all the common code needed by this plugin.  |
-// +---------------------------------------------------------------------------+
-// | Copyright (C) 2016 by the following authors:                              |
-// |                                                                           |
-// | Authors: Ben - ben AT geeklog DOT fr                                      |
-// +---------------------------------------------------------------------------+
-// | Created with the Geeklog Plugin Toolkit.                                  |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is free software; you can redistribute it and/or             |
-// | modify it under the terms of the GNU General Public License               |
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-// | GNU General Public License for more details.                              |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
-// +---------------------------------------------------------------------------+
-
-/**
-* @package Contact
-*/
-
-if (strpos(strtolower($_SERVER['PHP_SELF']), 'install_defaults.php') !== false) {
+if (isset($_SERVER['PHP_SELF']) && strpos(strtolower($_SERVER['PHP_SELF']), 'install_defaults.php') !== false) {
     die('This file can not be used on its own!');
 }
 
-/*
- * contact default settings
- *
- * Initial Installation Defaults used when loading the online configuration
- * records. These settings are only used during the initial installation
- * and not referenced any more once the plugin is installed
- *
- */
- 
- 
 global $_CONTACT_DEFAULT;
-
 $_CONTACT_DEFAULT = array();
 
-$_CONTACT_DEFAULT['contact_page'] = 'contact'; //id of the staticpage e.g. formmail 
-$_CONTACT_DEFAULT['use_contact_form'] = 1;
-$_CONTACT_DEFAULT['form_recipient'] = 2; // uid of the user recipient
-
-// This lets you select which functions are available for registered users only. 
-// When set to 1 will only allow logged-in users to access contact
 $_CONTACT_DEFAULT['contactloginrequired'] = 0;
+$_CONTACT_DEFAULT['hidecontactmenu'] = 0;
+$_CONTACT_DEFAULT['showleftblocks1'] = 1;
+$_CONTACT_DEFAULT['showrightblocks1'] = 0;
+$_CONTACT_DEFAULT['menu'] = 'Contact';
+$_CONTACT_DEFAULT['message'] = 'Thanks for your message.';
+$_CONTACT_DEFAULT['contact_page'] = 'contact';
+$_CONTACT_DEFAULT['contact_page_footer'] = '';
+$_CONTACT_DEFAULT['use_contact_form'] = 1;
+$_CONTACT_DEFAULT['form_recipient'] = 2;
+$_CONTACT_DEFAULT['allow_cc'] = 1;
+$_CONTACT_DEFAULT['show_subject'] = 1;
+$_CONTACT_DEFAULT['subject_prefix'] = '';
+$_CONTACT_DEFAULT['protection_mode'] = 0;
+$_CONTACT_DEFAULT['min_submit_seconds'] = 2;
+$_CONTACT_DEFAULT['max_message_length'] = 10000;
+$_CONTACT_DEFAULT['privacy_enabled'] = 0;
+$_CONTACT_DEFAULT['privacy_text'] = 'I have read the privacy policy.';
+$_CONTACT_DEFAULT['privacy_url'] = '';
 
-// Set to 1 to hide the "contact" entry from the top menu:
-$_CONTACT_DEFAULT['hidecontactmenu']      = 0;
-
-// Set to 0 will hide the left blocks on plugin page
-$_CONTACT_DEFAULT['showleftblocks1']   = 1;
-$_CONTACT_DEFAULT['showrightblocks1']  = 0;
-
-
-
-/**
-* Initialize contact plugin configuration
-*
-* Creates the database entries for the configuation if they don't already
-* exist. 
-*
-* @return   boolean     true: success; false: an error occurred
-*
-*/
 function plugin_initconfig_contact()
 {
-    global $_CONF, $_CONTACT_DEFAULT;
+    global $_CONTACT_DEFAULT;
 
     $c = config::get_instance();
     if (!$c->group_exists('contact')) {
+        $c->add('sg_0', NULL, 'subgroup', 0, 0, NULL, 0, true, 'contact');
 
-        //This is main subgroup #0
-		$c->add('sg_0', NULL, 'subgroup', 0, 0, NULL, 0, true, 'contact');
-		
-		//This is fieldset #1  in subgroup #0   
-		$c->add('fs_01', NULL, 'fieldset', 0, 0, NULL, 0, true, 'contact');
-        $c->add('contactloginrequired', $_CONTACT_DEFAULT['contactloginrequired'],
-                'select', 0, 0, 0, 10, true, 'contact');
-        $c->add('hidecontactmenu', $_CONTACT_DEFAULT['hidecontactmenu'], 'select',
-		        0, 0, 0, 20, true, 'contact');
-		$c->add('showleftblocks1', $_CONTACT_DEFAULT['showleftblocks1'], 'select',
-                0, 0, 0, 30, true, 'contact');
-		$c->add('showrightblocks1', $_CONTACT_DEFAULT['showrightblocks1'], 'select',
-                0, 0, 0, 40, true, 'contact');
-		$c->add('folder_name', 'contact', 'text',
-                0, 0, 0, 50, true, 'contact');
+        $c->add('fs_01', NULL, 'fieldset', 0, 0, NULL, 0, true, 'contact');
+        $c->add('contactloginrequired', $_CONTACT_DEFAULT['contactloginrequired'], 'select', 0, 0, 0, 10, true, 'contact');
+        $c->add('hidecontactmenu', $_CONTACT_DEFAULT['hidecontactmenu'], 'select', 0, 0, 0, 20, true, 'contact');
+        $c->add('showleftblocks1', $_CONTACT_DEFAULT['showleftblocks1'], 'select', 0, 0, 0, 30, true, 'contact');
+        $c->add('showrightblocks1', $_CONTACT_DEFAULT['showrightblocks1'], 'select', 0, 0, 0, 40, true, 'contact');
 
-				
-		$c->add('fs_02', NULL, 'fieldset', 0, 2, NULL, 0, true, 'contact');
-		$c->add('menu', 'Contact', 'text',
-                0, 2, 0, 5, true, 'contact');
-		$c->add('message', 'Thanks for any feedback', 'text',
-                0, 2, 0, 10, true, 'contact');
-		$c->add('contact_page', $_CONTACT_DEFAULT['contact_page'], 'text',
-                0, 2, 0, 20, true, 'contact');
-		$c->add('contact_page_footer', '', 'text',
-                0, 2, 0, 25, true, 'contact');
-		$c->add('use_contact_form', $_CONTACT_DEFAULT['use_contact_form'], 'select',
-                0, 2, 0, 30, true, 'contact');
-		$c->add('form_recipient', $_CONTACT_DEFAULT['form_recipient'], 'text',
-                0, 2, 0, 40, true, 'contact');
-				
+        $c->add('fs_02', NULL, 'fieldset', 0, 2, NULL, 0, true, 'contact');
+        $c->add('menu', $_CONTACT_DEFAULT['menu'], 'text', 0, 2, 0, 5, true, 'contact');
+        $c->add('message', $_CONTACT_DEFAULT['message'], 'text', 0, 2, 0, 10, true, 'contact');
+        $c->add('contact_page', $_CONTACT_DEFAULT['contact_page'], 'text', 0, 2, 0, 20, true, 'contact');
+        $c->add('contact_page_footer', $_CONTACT_DEFAULT['contact_page_footer'], 'text', 0, 2, 0, 25, true, 'contact');
+        $c->add('use_contact_form', $_CONTACT_DEFAULT['use_contact_form'], 'select', 0, 2, 0, 30, true, 'contact');
+        $c->add('form_recipient', $_CONTACT_DEFAULT['form_recipient'], 'text', 0, 2, 0, 40, true, 'contact');
+
+        $c->add('fs_03', NULL, 'fieldset', 0, 3, NULL, 0, true, 'contact');
+        $c->add('allow_cc', $_CONTACT_DEFAULT['allow_cc'], 'select', 0, 3, 0, 10, true, 'contact');
+        $c->add('show_subject', $_CONTACT_DEFAULT['show_subject'], 'select', 0, 3, 0, 20, true, 'contact');
+        $c->add('subject_prefix', $_CONTACT_DEFAULT['subject_prefix'], 'text', 0, 3, 0, 30, true, 'contact');
+        $c->add('protection_mode', $_CONTACT_DEFAULT['protection_mode'], 'select', 0, 3, 2, 40, true, 'contact');
+        $c->add('min_submit_seconds', $_CONTACT_DEFAULT['min_submit_seconds'], 'text', 0, 3, 0, 50, true, 'contact');
+        $c->add('max_message_length', $_CONTACT_DEFAULT['max_message_length'], 'text', 0, 3, 0, 60, true, 'contact');
+
+        $c->add('fs_04', NULL, 'fieldset', 0, 4, NULL, 0, true, 'contact');
+        $c->add('privacy_enabled', $_CONTACT_DEFAULT['privacy_enabled'], 'select', 0, 4, 0, 10, true, 'contact');
+        $c->add('privacy_text', $_CONTACT_DEFAULT['privacy_text'], 'text', 0, 4, 0, 20, true, 'contact');
+        $c->add('privacy_url', $_CONTACT_DEFAULT['privacy_url'], 'text', 0, 4, 0, 30, true, 'contact');
     }
 
     return true;
 }
-
-?>
